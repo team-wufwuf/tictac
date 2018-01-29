ef59cea2ee71349d40f86c52c86ce76c532bd7d9
Matthew Charles Baker <fimbaz@gmail.com>
2018-01-28 19:47:58 -0800

    add block class

--- appendlog.rb
+++ appendlog.rb
@@ -4,12 +4,27 @@ require 'base64'
 require 'json'
 require_relative 'tictac'
 module TicTac
+  class Block
+    def initialize(ipfs_addr)
+      @block=JSON.parse(%x(ipfs cat #{ipfs_addr}),symbolize_names: true)
+      @payload=Base64.decode64(@block[:payload])
+      @signature=Base64.decode64(@block[:signature])
+      @signer=@block[:signer]
+      @prev=@block[:prev]
+    end
+    def signed?
+      key=OpenSSL::PKey::RSA.new(@signer)
+      digest_algo=OpenSSL::Digest::SHA256.new
+      key.verify(digest_algo,@signature, @payload)
+    end
+  end
+    
   class AppendLog
     def initialize(initial_state=Empty_log)
       @pkey=OpenSSL::PKey::RSA.new(File.read(Private_key))
       @log=[]
       if initial_state == Empty_log
-        obj={last_log: nil, payload: ''}
+        obj={prev: nil, payload: ''}
         @log.push(signed_obj(obj))
       else
         #TODO verification of chain so far
@@ -17,11 +32,13 @@ module TicTac
       
     end
     def verify_entry(log_entry)
-      
+      proposed_head=JSON.parse(%x(ipfs cat #{log_entry}),symbolize_names: true)
+      payload=Base64.base64decode(proposed_head['payload'])
+      proposed_head['signature']
     end
     def new_entry(obj)
       last_payload=JSON.parse(%x(ipfs cat #{@log.last}),symbolize_names: true)
-      obj[:last_log]=@log.last
+      obj[:prev]=@log.last
       sobj=signed_obj(obj)
       @log.push(sobj).last
     end
@@ -39,5 +56,5 @@ module TicTac
   end
 end
 a=TicTac::AppendLog.new
-print a.new_entry({"hello":"World"})
+print TicTac::Block.new(a.new_entry({"hello":"World"})).signed?
   
