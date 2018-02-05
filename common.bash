c542b952d2fbbfbc06ef296423dddd3233164efc
Matthew Charles Baker <fimbaz@gmail.com>
2018-02-04 22:06:26 -0800

    fix bugs

--- common.bash
+++ common.bash
@@ -1,10 +1,9 @@
-export DEFAULT_ENEMY=QmdAXcov2rkPpP2UfD9Ei6MwW3ExWASuJAu87ywhENmTDN
 function ttt () {
     MY_ID="$2"
     OLD_CURRENT_GAME=$CURRENT_GAME
     case $1 in 
 	init)
-	    CURRENT_GAME=$(ruby bin/ttt.rb -i ${MY_ID} -o $DEFAULT_ENEMY)
+	    CURRENT_GAME=$(ruby bin/ttt.rb -i ${MY_ID} -o ${3})
 	    ;;
 	play)
 	    CURRENT_GAME=$(ruby bin/ttt.rb -i ${MY_ID} -g $CURRENT_GAME -m "$3")
