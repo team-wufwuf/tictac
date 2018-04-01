4c46f4ca38dbc471523b324c297339d5ee8dfa35
Ben C Lewis <benjamin.connah.lewis@gmail.com>
2018-04-01 15:57:47 +0100

    oops

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 lib = File.expand_path('../lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
-Rspec.configure do |c|
+RSpec.configure do |c|
   c.after do
     # need to release the locks on users between requests
     `ipfs repo fsck`
