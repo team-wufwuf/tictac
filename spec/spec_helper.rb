ecac24d126c6ef381ecd2b2f336b95e498bda491
Matthew Baker <fimbaz@gmail.com>
2018-04-02 02:05:01 +0000

    wait 20 secs or ipfs before starting tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,17 @@
 lib = File.expand_path('../lib', __dir__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
+def check_ipfs
+  ipfs_info=JSON.parse(`ipfs id`,symbolize_names: true)
+  ipfs_info[:Addresses].find do |address|
+    address =~ /ip4/
+  end
+end
 RSpec.configure do |c|
   c.before(:all) do
     Timeout.timeout(20) do
       Kernel.loop do
-        ipfs_info=JSON.parse(`ipfs id`,symbolize_names: true)
-        ipfs_info[:Addresses].find do |address|
-          address =~ /ip4/
-        end && break
-        sleep 1
+        check_ipfs && break
       end
     end
     # need to release the locks on users between requests
